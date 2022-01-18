68fd55090511b4983becec4fc1f303cf65c86780
mbayedione10 <mbayedione10@gmail.com>
2022-01-18 15:42:57 +0000

    calculate the absolute difference between the sums of matrix's diagonals

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PythonCodingChallenge
 Problem Solving, Data Structure, Python Challenges
+
+
+## Links  
+<!-- TOC -->
+- [1. Diagonal Matrix Challenge]()   
+    - [diagonalDifference Challenge](https://www.hackerrank.com/challenges/diagonal-difference/problem)  
+    - [Solution]()
+<!-- /TOC -->
+
---
+++ diagonalDifference.py
@@ -0,0 +1,33 @@
+def diagonalDifference():
+    """
+    Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+    :return: int: the absolute diagonal difference
+    """
+    n = int(input("The number of rows and columns in the square matrix"))
+    arr = [[0 for _ in range(n)] for _ in range(n)]
+    print(arr)
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+            arr[i][j] = float(input("arr[{}][{}]= ".format(i, j)))
+    left_diag = 0
+    right_diag = 0
+    i = 0
+    j = 0
+    k = n - 1
+    while i < n:
+        if arr[i][j] == arr[j][i]:
+            left_diag = left_diag + arr[i][j]
+        right_diag = right_diag + arr[i][k]
+        i = i + 1
+        j = j + 1
+        k = k - 1
+    if right_diag > left_diag:
+        diff = right_diag - left_diag
+    else:
+        diff = left_diag - right_diag
+    return int(diff)
+
+
+if __name__ == '__main__':
+    print(diagonalDifference())
